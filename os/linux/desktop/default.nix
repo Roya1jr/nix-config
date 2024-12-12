280aa11868e7df9a7e3b26029d38e11cad6593f3
Prince Junior Mguni <prince.mguni@outlook.com>
2024-12-12 21:22:07 +0200

    added jellyfin

--- os/linux/desktop/default.nix
+++ os/linux/desktop/default.nix
@@ -47,6 +47,25 @@
       ################
     };
 
+ ###Settings####
+  programs = {
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    };
+  };
+  ###Settings####
+
+  ###Services####
+  services.jellyfin = {
+    enable = true;
+    openFirewall = true;
+    user = "prince";
+  };
+  services.xserver.videoDrivers = [ "amdgpu" ];
+  ##############
+
   programs = {
     zsh.enable = true;
   };
