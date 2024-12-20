db1329b29ae908d6da9e54b5d1e7062dfcc0e129
Prince Junior Mguni <prince.mguni@outlook.com>
2024-12-20 12:33:30 +0200

    added noto fonts

--- home-manager/fonts.nix
+++ home-manager/fonts.nix
@@ -1,9 +1,10 @@
 { pkgs, ... }:
 
 with pkgs; [
-  nerd-fonts.fira-code
-  nerd-fonts.fantasque-sans-mono
   hack-font
   julia-mono
+  nerd-fonts.fira-code
+  nerd-fonts.fantasque-sans-mono
+  noto-fonts
   redhat-official-fonts
 ]
