95ac63cc6165a2bf2e22aac71c821a0f083de0cf
Prince Junior Mguni <prince.mguni@outlook.com>
2024-12-24 08:31:39 +0200

    added kde

--- os/linux/desktop/default.nix
+++ os/linux/desktop/default.nix
@@ -64,6 +64,14 @@
     user = "prince";
   };
   services.xserver.videoDrivers = [ "amdgpu" ];
+  services.xserver.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+  plasma-browser-integration
+  konsole
+  oxygen
+  ];
   ##############
 
   programs = {
