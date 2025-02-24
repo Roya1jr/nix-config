79853c41a1fc4b48ea274afc8f648201df93ceea
Prince Junior Mguni <prince.mguni@outlook.com>
2025-02-24 22:28:54 +0200

    added wayland to kde  plasma

--- os/linux/desktop/default.nix
+++ os/linux/desktop/default.nix
@@ -73,8 +73,8 @@
     user = "prince";
   };
   services.xserver.videoDrivers = [ "amdgpu" ];
-  services.xserver.enable = true;
   services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
   plasma-browser-integration
