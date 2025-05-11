bde93a2a22ecdd0a6e6444bba3a6adaf4be2d693
Prince Junior Mguni <prince.mguni@outlook.com>
2025-05-11 09:10:01 +0200

    removed nerdfonts

--- home-manager/fonts.nix
+++ home-manager/fonts.nix
@@ -4,7 +4,6 @@ with pkgs;
 [
   pkgs-unstable.hack-font
   pkgs-unstable.julia-mono
-  pkgs.nerd-fonts.droid-sans-mono
   pkgs-unstable.noto-fonts
   pkgs-unstable.redhat-official-fonts
   pkgs-unstable.uiua386
