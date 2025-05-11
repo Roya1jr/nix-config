1af66291f4c60f9314d5eaef3b28c67bfe78aaaf
Prince Junior Mguni <prince.mguni@outlook.com>
2025-05-11 09:08:59 +0200

    changed nerdfonts

--- home-manager/fonts.nix
+++ home-manager/fonts.nix
@@ -4,7 +4,7 @@ with pkgs;
 [
   pkgs-unstable.hack-font
   pkgs-unstable.julia-mono
-  pkgs-unstable.nerdfonts
+  nerd-fonts.droid-sans-mono
   pkgs-unstable.noto-fonts
   pkgs-unstable.redhat-official-fonts
   pkgs-unstable.uiua386
