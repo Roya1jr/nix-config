d2dd4aca3819038a42d65a6772daa3b7b229d4ee
Prince Junior Mguni <princem@bestforu.co.za>
2025-09-29 12:17:07 +0200

    updated

--- os/linux/shared.nix
+++ os/linux/shared.nix
@@ -1,57 +1,82 @@
-{ pkgs, pkgs-unstable, user, lib, ... }:
-
-let
-  sharedPackages = with pkgs; [
-    # CLI
-    fontfor
-    traceroute
-    pkgs-unstable.ollama
-
-    # Programming
-    glibc
-    gfortran14
-    libclang
-    xorg.libX11
-
-    pkgs-unstable.factor-lang
-    hare
-    (lib.hiPrio pkgs-unstable.j)
-
-    # Broken / commented packages
-    # io
-    # qnial
-    racket
-  ];
-in
 {
-  # System services and settings
+  pkgs,
+  pkgs-unstable,
+  inputs,
+  lib,
+  ...
+}:
+{
   services.printing.enable = true;
   services.openssh.enable = true;
   networking.hostName = "nixos";
   networking.networkmanager.enable = true;
   time.timeZone = "Africa/Johannesburg";
   i18n.defaultLocale = "en_ZA.UTF-8";
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
   virtualisation.containers.enable = true;
-
-  # User accounts
-  users.mutableUsers = true;
-  users.users.${user} = {
-    shell = pkgs.zsh;
-    description = "Me";
-    isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" "dialout" ];
+  users = {
+    mutableUsers = true;
+    users = {
+      prince = {
+        shell = pkgs.zsh;
+        description = "Me";
+        isNormalUser = true;
+        extraGroups = [
+          "wheel"
+          "networkmanager"
+          "dialout"
+        ];
+      };
+    };
   };
 
-  # Programs
-  programs.zsh.enable = true;
-  programs.nix-ld.enable = true;
-
-  # Home-manager for the shared packages
-  home-manager.users.${user} = { system, pkgs-unstable, ... }:
-  {
-    home.packages = sharedPackages;
-    home.file = { };
+  programs = {
+    zsh = {
+      enable = true;
+    };
+    nix-ld.enable = true;
   };
+  home-manager.users.prince =
+    { system, pkgs-unstable, ... }:
+    {
+      home = {
+        file = {
+
+        };
+        packages = with pkgs; [
+          #######CLI##########
+          fontfor
+          traceroute
+          pkgs-unstable.ollama
+          ######################
+
+          #######Programming#########
+
+          ##C++#
+          glibc
+          gfortran14
+          libclang
+          xorg.libX11
+          ######
+
+          pkgs-unstable.factor-lang
+          hare
+          (lib.hiPrio pkgs-unstable.j)
+
+          ##Broken
+          #io
+
+          #Broken
+          ##qnial
+          racket
+
+          ####################
+        ];
+      };
+
+    };
+
 }
-
