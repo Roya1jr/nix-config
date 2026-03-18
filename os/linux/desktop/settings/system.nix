43f87914cd4ec8425d32354c75a4e9267fb4c528
Prince Junior Mguni <prince.mguni@outlook.com>
2026-03-18 21:10:48 +0200

    updated mesa

--- os/linux/desktop/settings/system.nix
+++ os/linux/desktop/settings/system.nix
@@ -19,6 +19,7 @@
     enableRedistributableFirmware = true;
     graphics = {
       enable = true;
+      enable32Bit = true;
       extraPackages = with pkgs; [
         intel-media-driver # VA-API (iHD) userspace
         vpl-gpu-rt # oneVPL (QSV) runtime
--- overlays/pkgs.nix
+++ overlays/pkgs.nix
@@ -3,4 +3,7 @@ inputs: final: prev: {
   # BQN LSP
   bqnlsp = inputs.bqn-lsp.packages.${prev.system}.lsp;
 
+  # Latest Mesa from unstable
+  mesa = inputs.nixpkgs-unstable.legacyPackages.${prev.system}.mesa;
+
 }
